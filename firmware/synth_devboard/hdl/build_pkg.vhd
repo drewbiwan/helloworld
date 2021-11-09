beb07bc3fff0c993a01d8285e1cf6040d6924584
Drew Coker <drew.coker@gmail.com>
2021-11-09 10:35:50 -0500

    POST-SYNTHESIS AUTOCOMMIT. Ran from compile.tcl.

--- firmware/synth_devboard/hdl/build_pkg.vhd
+++ firmware/synth_devboard/hdl/build_pkg.vhd
@@ -11,10 +11,10 @@ use ieee.std_logic_1164.all;
 package build_pkg is
     constant MAJOR_VERSON_C     : integer := 0;
     constant MINOR_VERSON_C     : integer := 0;
-    constant BUILD_NUMBER_C     : integer := 30;
-    constant BUILD_TIME_BCD_C   : STD_LOGIC_VECTOR(31 downto 0) := x"21110810";
-    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"6189412B";
+    constant BUILD_NUMBER_C     : integer := 31;
+    constant BUILD_TIME_BCD_C   : STD_LOGIC_VECTOR(31 downto 0) := x"21110910";
+    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"618A954D";
     constant BRANCH_STRING_C    : string := "refs/heads/main";
-    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"90c3f27f0f3ba12fd2dafb0940714ebbc107bb07";
+    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"2aea67f4a9f60d655acf26753774087b05cf1c96";
     constant HARDWARE_STRING_C    : string := "DEV0";
 end build_pkg;
