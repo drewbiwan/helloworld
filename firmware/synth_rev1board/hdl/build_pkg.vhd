bc1e1f73a7c89308d00507aec13ae99c715d7321
Drew Coker <drew.coker@gmail.com>
2021-11-09 16:32:36 -0500

    POST-SYNTHESIS AUTOCOMMIT. Ran from compile.tcl.

--- firmware/synth_rev1board/hdl/build_pkg.vhd
+++ firmware/synth_rev1board/hdl/build_pkg.vhd
@@ -9,13 +9,13 @@ library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 package build_pkg is
-    constant CONFIGURATION_NAME_C   : integer := "rev1board;
+    constant CONFIGURATION_NAME_C   : string := "rev1board";
     constant MAJOR_VERSON_C         : integer := 0;
     constant MINOR_VERSON_C         : integer := 0;
     constant BUILD_NUMBER_C         : integer := 1;
-    constant BUILD_TIME_BCD_C       : STD_LOGIC_VECTOR(31 downto 0) := x"21110915";
-    constant BUILD_TIME_EPOCH_C     : STD_LOGIC_VECTOR(31 downto 0) := x"618AE124";
+    constant BUILD_TIME_BCD_C       : STD_LOGIC_VECTOR(31 downto 0) := x"21110916";
+    constant BUILD_TIME_EPOCH_C     : STD_LOGIC_VECTOR(31 downto 0) := x"618AE8EC";
     constant BRANCH_STRING_C        : string := "refs/heads/main";
-    constant COMMIT_OID_C           : STD_LOGIC_VECTOR(159 downto 0) := x"7ebf0e482f7fc86d73304ebf04334409584a377d";
+    constant COMMIT_OID_C           : STD_LOGIC_VECTOR(159 downto 0) := x"a567d484f24d9af320bce93c633afa663d93be62";
     constant CONFIGURATION_STRING_C : string := "TEMP";
 end build_pkg;
