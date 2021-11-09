21a20ef3ecbee123eb0e3b334e8421dfe566c2d7
Drew Coker <drew.coker@gmail.com>
2021-11-09 14:48:08 -0500

    PRE-SYNTHESIS AUTOCOMMIT. Ran from compile.tcl.

--- firmware/synth_rev1board/hdl/build_pkg.vhd
+++ firmware/synth_rev1board/hdl/build_pkg.vhd
@@ -11,10 +11,10 @@ use ieee.std_logic_1164.all;
 package build_pkg is
     constant MAJOR_VERSON_C     : integer := 0;
     constant MINOR_VERSON_C     : integer := 0;
-    constant BUILD_NUMBER_C     : integer := 2;
+    constant BUILD_NUMBER_C     : integer := 3;
     constant BUILD_TIME_BCD_C   : STD_LOGIC_VECTOR(31 downto 0) := x"21110914";
-    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"618ACF5A";
+    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"618AD070";
     constant BRANCH_STRING_C    : string := "refs/heads/main";
-    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"bd754906ad754577b52ad965ad7d2a99f9d8cd21";
+    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"de5e3cbe115bd162bb57faa1f285809a5ddacf3e";
     constant HARDWARE_STRING_C    : string := "TEMP";
 end build_pkg;
