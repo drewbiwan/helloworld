e0b3315a829dc0f07c176a337cb82d37fc7f69df
Drew Coker <drew.coker@gmail.com>
2021-11-09 14:14:15 -0500

    PRE-SYNTHESIS AUTOCOMMIT. Ran from compile.tcl.

---
+++ firmware/synth_devboard_rev1/clean.sh
@@ -0,0 +1,11 @@
+rm -d -r *.hw
+rm -d -r *.cache
+rm -d -r *.runs
+rm -d -r *.gen
+rm -d -r *.ip_user_files
+rm -d -r *.sim
+rm -d -r *.srcs
+rm -d -r bd/*/
+rm *.xpr
+rm *.jou
+rm *.log
--- firmware/synth_devboard_rev1/hdl/build_pkg.vhd
+++ firmware/synth_devboard_rev1/hdl/build_pkg.vhd
@@ -11,10 +11,10 @@ use ieee.std_logic_1164.all;
 package build_pkg is
     constant MAJOR_VERSON_C     : integer := 0;
     constant MINOR_VERSON_C     : integer := 0;
-    constant BUILD_NUMBER_C     : integer := 33;
-    constant BUILD_TIME_BCD_C   : STD_LOGIC_VECTOR(31 downto 0) := x"21110913";
-    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"618AC07A";
+    constant BUILD_NUMBER_C     : integer := 2;
+    constant BUILD_TIME_BCD_C   : STD_LOGIC_VECTOR(31 downto 0) := x"21110914";
+    constant BUILD_TIME_EPOCH_C : STD_LOGIC_VECTOR(31 downto 0) := x"618AC882";
     constant BRANCH_STRING_C    : string := "refs/heads/main";
-    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"af7f98e09af4df0c4148fd1eb1db517d3349d507";
-    constant HARDWARE_STRING_C    : string := "DEV0";
+    constant COMMIT_OID_C       : STD_LOGIC_VECTOR(159 downto 0) := x"53bcf6c624aa421c876f519d290316808a36fb5f";
+    constant HARDWARE_STRING_C    : string := "TEMP";
 end build_pkg;
